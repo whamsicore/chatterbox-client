67c052ba5f53c792744eaa15f79bcc4c10275dfa
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 21:13:41 -0700

    Added befriending feature Added message in chatroom feature Fixed bug for All Rooms display
    Need to: user generated room name

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,5 @@
 // YOUR CODE HERE:
-
+window.friends = {};
 var app = {}
 app.init = function() {
   var rooms = findRooms(window.data.results);
@@ -38,7 +38,7 @@ app.fetch = function() {
 };
 
 var printRooms = function(rooms) {
-  $('#chatrooms').html('');
+  $('#chatrooms').html(''); //clear previous options
   // $('#chatrooms').append("<option value='All Rooms'>All Rooms</option>")
   var defaultOption = $('<option></option>').val('All Rooms').text('All Rooms'); 
   $('#chatrooms').append(defaultOption);
@@ -46,6 +46,8 @@ var printRooms = function(rooms) {
     var newOptions = $('<option></option>').val(key).text(key);
     $('#chatrooms').append(newOptions);
   }
+  var fakeOption = $('<option></option>').val('').text('-------------'); 
+  $('#chatrooms').prepend(fakeOption);
 };
 
 var findRooms = function(messageArray) { //should accept results array from window.data
@@ -54,8 +56,10 @@ var findRooms = function(messageArray) { //should accept results array from wind
   for(var i = 0; i < messageArray.length; i++) {
     var message = messageArray[i];
     var key = message.roomname;
-    result[key] = key;
-  }
+    if(key!==undefined && key!==null && key!==''){
+      result[key] = key;
+    } //if
+  } //for
   // console.log('result:'+JSON.stringify(result));
   return result;
 };
@@ -63,10 +67,11 @@ var findRooms = function(messageArray) { //should accept results array from wind
 setInterval(app.fetch, 1000); //display messages
 
 app.fetch();
+
 setTimeout(function(){
   app.init();
 
-}, 1000);
+}, 1500);
 
 var displayPosts = function(data, roomname){
   $('#messages').html('');
@@ -76,17 +81,22 @@ var displayPosts = function(data, roomname){
   for(var i = 0; i<results.length;i++){
     var chat = results[i];
     // console.log(results[i]);
-    if(chat['roomname'] === window.chatRoom) {
-      var newDiv = $('<div></div>').text(chat['username']+': '+chat["text"]);
-      $('#messages').append(newDiv);
-    }else if(window.chatRoom === 'All Rooms'){
-      var newDiv = $('<div></div>').text(chat['username']+': '+chat["text"]);
-      $('#messages').append(newDiv);
-
-     //if
-    }
+      var newDiv = $('<div></div>');
+      var nameSpan = $("<span class='chatName'></span>").text(chat['username']);
+      var textSpan = $("<span class='chatText'></span>").text(": "+chat['text']);
+      newDiv.append(nameSpan).append(textSpan);
+      
+      if(chat['username'] in window.friends){// note: if chat['username'] is inside window.friends object, bold the newDiv element by adding 'friend' class
+        console.log('adding class to user: '+chat['username']);
+        newDiv.addClass('friends');
+      }
     
-  }
+    if(chat['roomname'] === window.chatRoom) {
+      $('#messages').append(newDiv);
+    }else if(window.chatRoom === undefined || window.chatRoom === 'All Rooms'){
+      $('#messages').append(newDiv);
+    } //if
+  } //for
 }; 
 
 //write message
@@ -95,7 +105,7 @@ $(document).on('click','#submit', function(){
   var message = {};
   message['username'] = 'P&C';
   message['text'] = input;
-  message['roomname'] = 'Payton And Chris Say Hi!';
+  message['roomname'] = window.chatRoom;
 
   if(message === '') {
     alert('Message is empty, please try again');
@@ -111,12 +121,24 @@ $(document).on('click', '#chatrooms', function(){
   app.init();
 });
 
+// func: Befriend user
+$(document).on('click', '#messages .chatName', function(){
+  var name = $(this).text();
+  console.log('chat clicked()'+name);
+  if(name in window.friends){
+    delete window.friends[name];
+  }else{
+    window.friends[name] = true; 
+  } //if
+  // app.init();
+});
+
 // $(document).on('click', '#chatrooms option', function(){
 //   console.log('clicked options');
 //   // app.init();
 // });
 
-$(document).on('click', '#chatrooms option', function(){
+$(document).on('change', 'select', function(){
   console.log('clicked options');
   window.chatRoom =  $("#chatrooms").val();
   // app.init();

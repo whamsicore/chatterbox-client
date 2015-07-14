d2eeae901f8ae2eed81d1dafb680c6564f731f6d
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 20:25:08 -0700

    chat rooms almost works, only ALL ROOMS option is not working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,11 @@
 // YOUR CODE HERE:
 
 var app = {}
-app.init = function() {};
+app.init = function() {
+  var rooms = findRooms(window.data.results);
+  printRooms(rooms);
+};
+
 app.send = function(postPackage) { // message = {username, text, roomname}
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
@@ -10,7 +14,7 @@ app.send = function(postPackage) { // message = {username, text, roomname}
     data: JSON.stringify(postPackage),
     contentType: 'application/json', //what does this mean?
     success: function (data) {
-      console.log('chatterbox: Message sent: '+JSON.stringify(data));
+      // console.log('chatterbox: Message sent: ');
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -26,87 +30,98 @@ app.fetch = function() {
     type: 'GET',
     success: function(data){
       displayPosts(data);
+      window.data = data; 
       // console.log('Success');
     },
     dataType: 'json'
   });
 };
 
+var printRooms = function(rooms) {
+  $('#chatrooms').html('');
+  // $('#chatrooms').append("<option value='All Rooms'>All Rooms</option>")
+  var defaultOption = $('<option></option>').val('All Rooms').text('All Rooms'); 
+  $('#chatrooms').append(defaultOption);
+  for (var key in rooms) {
+    var newOptions = $('<option></option>').val(key).text(key);
+    $('#chatrooms').append(newOptions);
+  }
+};
+
+var findRooms = function(messageArray) { //should accept results array from window.data
+  var result = {};
+  // debugger
+  for(var i = 0; i < messageArray.length; i++) {
+    var message = messageArray[i];
+    var key = message.roomname;
+    result[key] = key;
+  }
+  // console.log('result:'+JSON.stringify(result));
+  return result;
+};
+
 setInterval(app.fetch, 1000); //display messages
 
-var displayPosts = function(data){
+app.fetch();
+setTimeout(function(){
+  app.init();
+
+}, 1000);
+
+var displayPosts = function(data, roomname){
   $('#messages').html('');
   var results = data.results;
+
 
   for(var i = 0; i<results.length;i++){
     var chat = results[i];
     // console.log(results[i]);
-    var newDiv = $('<div></div>').text(chat['username']+': '+chat["text"]);
-    $('#messages').append(newDiv);
+    if(chat['roomname'] === window.chatRoom) {
+      var newDiv = $('<div></div>').text(chat['username']+': '+chat["text"]);
+      $('#messages').append(newDiv);
+    }else if(window.chatRoom === 'All Rooms'){
+      var newDiv = $('<div></div>').text(chat['username']+': '+chat["text"]);
+      $('#messages').append(newDiv);
+
+     //if
+    }
+    
   }
 }; 
 
 //write message
 $(document).on('click','#submit', function(){
   var input = $('input').val();
-  console.log(input);
   var message = {};
-  message['username'] = 'Traitor on floor 6';
+  message['username'] = 'P&C';
   message['text'] = input;
-  message['roomname'] = 'HR floor 8';
+  message['roomname'] = 'Payton And Chris Say Hi!';
 
   if(message === '') {
     alert('Message is empty, please try again');
     return;
   } else {
-    console.log(message);
     app.send(message);
     $('input').val('');
   }
 });
 
-var trollText = [
-"88888888888  88           ,ad8888ba,      ,ad8888ba,    88888888ba   ",
-"88           88          d8aa    aa8b    aaa    aa8b   88      a8b  ",
-"88           88         d8'        `8b  d8'        `8b  88      ,8P   ",
-"88aaaaa      88         88          88  88          88  88aaaaaa8P'  ",
-"88*****      88         88          88  88          88  88aaaa88'    ",
-"88           88         Y8,        ,8P  Y8,        ,8P  88    `8b    ",
-"88           88          Y8a.    .a8P    Y8a.    .a8P   88     `8b   ",
-"88           88888888888  `sY8888Yla      aaY8888Yaa    88      `8b  ",
-" ",
-" ",
-" ",
-" ad88888ba                                                           ",
-"d8     a8b                                                          ",
-"Y8a     a8P                                                          ",
-" aY8aaa8Pa                                                           ",
-" ,d8aaa8b,                                                           ",
-"d8a     a8b                                                          ",
-"Y8a     a8P                                                          ",
-" aY88888Pa                                                           ",
-" ",
-" ",
-" ",
-"88888888ba   88        88  88           88888888888  ad88888ba       ",
-"88      a8b  88        88  88           88          d81     28b      ",
-"88      a8P  88        88  88           88          Y8,              ",
-"88aaaaaa8P'  88        88  88           88aaaaa     `Y8aaaaa,       " ,
-"88aaaa88'    88        88  88           88aaaa       `aaaa8b,      ",
-"88    `8b    88        88  88           88                  `8b     ", 
-"88     `8b   Y8a.    .a8P  88           88          Y8a     a8P    "  ,
-"88      `8b   `aY8888Yaa   88888888888  88888888888  aY88888Pa    "
-];
+$(document).on('click', '#chatrooms', function(){
+  console.log('clicked');
+  app.init();
+});
 
-for(var i = 0; i<trollText.length;i++){
-  var message = {};
-  console.log(trollText[i]);
-  message['username'] = 'Traitor on floor 6';
-  message['text'] = trollText[i];
-  message['roomname'] = 'HR floor 8';
-  app.send(message);
-}
-//send test: 
+// $(document).on('click', '#chatrooms option', function(){
+//   console.log('clicked options');
+//   // app.init();
+// });
+
+$(document).on('click', '#chatrooms option', function(){
+  console.log('clicked options');
+  window.chatRoom =  $("#chatrooms").val();
+  // app.init();
+});
+
 /*
 
 {

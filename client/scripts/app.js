13fad84a3f923e0fc3a627cb69690fdfcea0077e
Hack Reactor Students <accounts@hackreactor.com>
2015-07-14 10:29:29 -0700

    chris has come

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,12 @@
 // YOUR CODE HERE:
 window.friends = {};
-var app = {}
+var app = {};
+
+app.username = '';
+
 app.init = function() {
-  var rooms = findRooms(window.data.results);
-  printRooms(rooms);
+  var rooms = app.findRooms(window.data.results);
+  app.printRooms(rooms);
 };
 
 app.send = function(postPackage) { // message = {username, text, roomname}
@@ -13,12 +16,12 @@ app.send = function(postPackage) { // message = {username, text, roomname}
     type: 'POST',
     data: JSON.stringify(postPackage),
     contentType: 'application/json', //what does this mean?
-    success: function (data) {
+    success: function(data) {
       // console.log('chatterbox: Message sent: ');
     },
-    error: function (data) {
+    error: function(data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message:'+JSON.stringify(data));
+      console.error('chatterbox: Failed to send message:' + JSON.stringify(data));
     }
   }); //ajax
 };
@@ -28,35 +31,48 @@ app.fetch = function() {
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    success: function(data){
-      displayPosts(data);
-      window.data = data; 
+    success: function(data) {
+      app.displayPosts(data);
+      window.data = data;
       // console.log('Success');
     },
     dataType: 'json'
   });
 };
 
-var printRooms = function(rooms) {
+app.login = function() {
+  $("#logindiv").css("display", "block");
+  $(document).on("click", "#login", function() {
+    user = $("#name").val();
+    if (user === "") {
+      user = "Guest";
+    }
+    app.username = user;
+    $("#logindiv").css("display", "none");
+    $("#username").text(app.username);
+  });
+};
+
+app.printRooms = function(rooms) {
   $('#chatrooms').html(''); //clear previous options
-  // $('#chatrooms').append("<option value='All Rooms'>All Rooms</option>")
-  var defaultOption = $('<option></option>').val('All Rooms').text('All Rooms'); 
+  var defaultOption = $('<option></option>').val('All Rooms').text('All Rooms');
   $('#chatrooms').append(defaultOption);
   for (var key in rooms) {
     var newOptions = $('<option></option>').val(key).text(key);
     $('#chatrooms').append(newOptions);
   }
-  var fakeOption = $('<option></option>').val('').text('-------------'); 
+  var fakeOption = $('<option></option>').val('').text('-------------');
   $('#chatrooms').prepend(fakeOption);
 };
 
-var findRooms = function(messageArray) { //should accept results array from window.data
+app.findRooms = function(messageArray) { //should accept results array from window.data
+
   var result = {};
   // debugger
-  for(var i = 0; i < messageArray.length; i++) {
+  for (var i = 0; i < messageArray.length; i++) {
     var message = messageArray[i];
     var key = message.roomname;
-    if(key!==undefined && key!==null && key!==''){
+    if (key !== undefined && key !== null && key !== '') {
       result[key] = key;
     } //if
   } //for
@@ -64,50 +80,52 @@ var findRooms = function(messageArray) { //should accept results array from wind
   return result;
 };
 
+app.login();
+
 setInterval(app.fetch, 1000); //display messages
 
 app.fetch();
 
-setTimeout(function(){
+setTimeout(function() {
   app.init();
 
 }, 1500);
 
-var displayPosts = function(data, roomname){
+app.displayPosts = function(data, roomname) {
   $('#messages').html('');
   var results = data.results;
 
 
-  for(var i = 0; i<results.length;i++){
+  for (var i = 0; i < results.length; i++) {
     var chat = results[i];
     // console.log(results[i]);
-      var newDiv = $('<div></div>');
-      var nameSpan = $("<span class='chatName'></span>").text(chat['username']);
-      var textSpan = $("<span class='chatText'></span>").text(": "+chat['text']);
-      newDiv.append(nameSpan).append(textSpan);
-      
-      if(chat['username'] in window.friends){// note: if chat['username'] is inside window.friends object, bold the newDiv element by adding 'friend' class
-        console.log('adding class to user: '+chat['username']);
-        newDiv.addClass('friends');
-      }
-    
-    if(chat['roomname'] === window.chatRoom) {
+    var newDiv = $('<div></div>');
+    var nameSpan = $("<span class='chatName'></span>").text(chat['username']);
+    var textSpan = $("<span class='chatText'></span>").text(": " + chat['text']);
+    newDiv.append(nameSpan).append(textSpan);
+
+    if (chat['username'] in window.friends) { // note: if chat['username'] is inside window.friends object, bold the newDiv element by adding 'friend' class
+      console.log('adding class to user: ' + chat['username']);
+      newDiv.addClass('friends');
+    }
+
+    if (chat['roomname'] === window.chatRoom) {
       $('#messages').append(newDiv);
-    }else if(window.chatRoom === undefined || window.chatRoom === 'All Rooms'){
+    } else if (window.chatRoom === undefined || window.chatRoom === 'All Rooms') {
       $('#messages').append(newDiv);
     } //if
   } //for
-}; 
+};
 
 //write message
-$(document).on('click','#submit', function(){
+$(document).on('click', '#submit', function() {
   var input = $('input').val();
   var message = {};
-  message['username'] = 'P&C';
+  message['username'] = app.username;
   message['text'] = input;
   message['roomname'] = window.chatRoom;
 
-  if(message === '') {
+  if (message === '') {
     alert('Message is empty, please try again');
     return;
   } else {
@@ -116,31 +134,26 @@ $(document).on('click','#submit', function(){
   }
 });
 
-$(document).on('click', '#chatrooms', function(){
+$(document).on('click', '#chatrooms', function() {
   console.log('clicked');
   app.init();
 });
 
 // func: Befriend user
-$(document).on('click', '#messages .chatName', function(){
+$(document).on('click', '#messages .chatName', function() {
   var name = $(this).text();
-  console.log('chat clicked()'+name);
-  if(name in window.friends){
+  console.log('chat clicked()' + name);
+  if (name in window.friends) {
     delete window.friends[name];
-  }else{
-    window.friends[name] = true; 
+  } else {
+    window.friends[name] = true;
   } //if
   // app.init();
 });
 
-// $(document).on('click', '#chatrooms option', function(){
-//   console.log('clicked options');
-//   // app.init();
-// });
-
-$(document).on('change', 'select', function(){
+$(document).on('change', 'select', function() {
   console.log('clicked options');
-  window.chatRoom =  $("#chatrooms").val();
+  window.chatRoom = $("#chatrooms").val();
   // app.init();
 });
 
@@ -156,6 +169,3 @@ test ObjectId:
     url: "https://api.parse.com/1/classes/chatterbox/QmqKIzbsT7
 
 */
-
-
-
